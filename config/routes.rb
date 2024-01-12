057e3875cf412a73c0812a6c56bda90fb1c27485
Iñaki Goyeneche <igoyeneche.98@gmail.com>
2024-01-12 16:47:08 -0300

    [feat] adding button to mock a webhook event

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :webhooks, only: %i[index show create destroy]
   post "webhooks/:source", to: "webhooks#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "home#index"
 
   # Defines the root path route ("/")
   # root "posts#index"
